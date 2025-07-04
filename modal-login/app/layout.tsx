500a91b719e46c1d329c8c442448186c99a96c2d
0xchai <r00tsec0.0@gmail.com>
2025-07-04 16:49:10 +0700

    Update layout.tsx

--- modal-login/app/layout.tsx
+++ modal-login/app/layout.tsx
@@ -1,12 +1,9 @@
 import { config } from "@/config";
 import { cookieToInitialState } from "@account-kit/core";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { headers } from "next/headers";
 import "./globals.css";
 import { Providers } from "./providers";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sign in to Gensyn Testnet",
@@ -18,8 +15,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // Persist state across pages
-  // https://accountkit.alchemy.com/react/ssr#persisting-the-account-state
   const initialState = cookieToInitialState(
     config,
     headers().get("cookie") ?? undefined,
@@ -27,7 +22,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Providers initialState={initialState}>{children}</Providers>
       </body>
     </html>
